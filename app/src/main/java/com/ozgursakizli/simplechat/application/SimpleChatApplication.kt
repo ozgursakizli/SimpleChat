c431afa938f601ae94a2b754cf912fd2ca47d275
ozgursakizli <ozgursakizli@gmail.com>
2021-03-13 18:06:26 +0300

    koin implementation

--- app/src/main/java/com/ozgursakizli/simplechat/application/SimpleChatApplication.kt
+++ app/src/main/java/com/ozgursakizli/simplechat/application/SimpleChatApplication.kt
@@ -1,11 +1,18 @@
 package com.ozgursakizli.simplechat.application
 
 import android.app.Application
+import com.ozgursakizli.simplechat.di.presentationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class SimpleChatApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@SimpleChatApplication)
+            presentationModule
+        }
     }
 
 }
---
+++ app/src/main/java/com/ozgursakizli/simplechat/di/PresentationModule.kt
@@ -0,0 +1,20 @@
+package com.ozgursakizli.simplechat.di
+
+import com.ozgursakizli.simplechat.ui.main.MainViewModel
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val presentationModule = module {
+    factory<CoroutineContext> {
+        SupervisorJob()
+    }
+    factory {
+        Dispatchers.IO
+    }
+    viewModel {
+        MainViewModel()
+    }
+}
